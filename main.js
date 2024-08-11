e72c6777a28854f65641672eacbfd2f759cb222c
Kane2881 <kanethepro2881@gmail.com>
2024-08-11 09:31:59 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-nav");
+const primaryHeader = document.querySelector(".primary-header");
+const navList = document.querySelector(".nav-list");
+
+navToggle.addEventListener("click", () => {
+  primaryHeader.hasAttribute("data-overlay")
+    ? navToggle.setAttribute("aria-expanded", true)
+    : navToggle.setAttribute("aria-expanded", false);
+
+  primaryHeader.hasAttribute("data-overlay")
+    ? navList.classList.add("active")
+    : navList.classList.remove("active");
+  primaryHeader.toggleAttribute("data-overlay");
+});
